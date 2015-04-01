d23e0482b1a06971a46252f5399e8343b440f19d
zanjie <zanjie@outlook.com>
2015-04-01 08:36:07 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zanjie::Application.config.secret_key_base = '51a4c7e3c04870634edfa190239e777f898ede9cbe0048d56e77b2aa2c821d022a6cb548845a7099d7af71eb1d234be5666c994b2288b51c75d73cbf9dc275e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zanjie::Application.config.session_store :cookie_store, key: '_zanjie_session'
